b3041d70cf11ecd120d14011b3aba4043539e1c8
Yulia Gapunovych <45093751+YulitaGap@users.noreply.github.com>
2020-04-01 21:44:13 +0300

    update stepan.js

--- src/lib/stepan.js
+++ src/lib/stepan.js
@@ -1,9 +1,17 @@
 
 export default class Stepan {
+  
+  static validateElement(elem){
+    return document.createElement(elem).toString() !== "[object HTMLUnknownElement]"
+  };
+
   static createElement(element, parent, attributes = {}) {
     // TODO: check if this is a valid tag name
-    const newElement = document.createElement(element);
 
+    if (!Stepan.validateElement(element)){
+      throw new StepanError("The tag of element is not valid.");
+    }
+    const newElement = document.createElement(element);
     const { innerHTML, innerText } = attributes;
 
     for (let attribute in attributes) {
@@ -24,13 +32,29 @@ export default class Stepan {
 
   static Component = class {
     constructor(parent) {
+      
 
-      // TODO: 1. Create StepanError class to define all framework errors
-      //       2. throw an error if parent is null or undefined, or if it's not a valid DOM object
+      if ( parent === null || parent === undefined){
+        // 2. throw an error if parent is null or undefinedx
 
+        throw new StepanError("The parent object is null of undefined.")
+      }
+      if (! parent instanceof Element){
+        //2. throw an error if parent is not a valid DOM object
+
+        throw new StepanError("The parent object is not a valid DOM object")
+      }
       this.parent = parent;
     }
-
     // TODO (Bonus): Ensure that every component returns a top-level root element
   }
 }
+
+// TODO: 1. Create StepanError class to define all framework errors
+class StepanError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "StepanError"
+  }
+}
+
